f9b5604e4e58b4f46f4c7bb2b0f90d10ecb26a3e
Marcin Drabek <marcindrabek@hotmail.com>
2023-06-27 18:45:04 +0200

    Added initial project

---
+++ ForeheadApi/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForeheadApi.Controllers;
+
+[ApiController]
+[Route("categories")]
+public class CategoryController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCategories()
+    {
+        return Ok(Array.Empty<string>());
+    }
+}
---
+++ ForeheadApi/Controllers/QuestionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForeheadApi.Controllers;
+
+[ApiController]
+[Route("questions")]
+public class QuestionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetQuestionForCategoryAsync([FromQuery] int categoryId)
+    {
+        if (categoryId == 0)
+        {
+            return BadRequest();
+        }
+
+        return Ok(Array.Empty<string>());
+    }
+}
