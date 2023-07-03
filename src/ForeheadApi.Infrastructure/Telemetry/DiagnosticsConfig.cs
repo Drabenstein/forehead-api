965f46d408fd81144dd18076afba164fc3a7591d
Marcin Drabek <marcindrabek@hotmail.com>
2023-07-03 02:00:50 +0200

    Fixed DeepSource issue

--- src/ForeheadApi.Infrastructure/Telemetry/DiagnosticsConfig.cs
+++ src/ForeheadApi.Infrastructure/Telemetry/DiagnosticsConfig.cs
@@ -5,5 +5,5 @@ namespace ForeheadApi.Infrastructure.Telemetry;
 public static class DiagnosticsConfig
 {
     public const string ServiceName = "ForeheadApi";
-    public static readonly ActivitySource ActivitySource = new ActivitySource(ServiceName);
+    public static readonly ActivitySource ActivitySource = new(ServiceName);
 }
